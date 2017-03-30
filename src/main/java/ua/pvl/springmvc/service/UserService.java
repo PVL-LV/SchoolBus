97ed67e9575171131ae7f7a700a35d0eeedbccbf
pvl <pvl-lv@mail.ru>
2017-03-30 16:40:25 +0300

    finish basic web structure

---
+++ src/main/java/ua/pvl/springmvc/service/UserService.java
@@ -0,0 +1,24 @@
+package ua.pvl.springmvc.service;
+
+import java.util.List;
+
+import ua.pvl.springmvc.model.User;
+
+
+public interface UserService {
+
+    User findById(int id);
+
+    User findBySSO(String sso);
+
+    void saveUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUserBySSO(String sso);
+
+    List<User> findAllUsers();
+
+    boolean isUserSSOUnique(Integer id, String sso);
+
+}
